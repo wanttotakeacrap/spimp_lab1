650a81becffbf5485cbcc2446ff8c2bbc506ba9c
wanttotakeacrap <dshkrsnnkv@mail.ru>
2026-05-25 14:48:50 +0300

    second fix

---
+++ Desktop/Новая папка/учеба/6 сем/спимп/laba1/src/laba1/core.clj
@@ -0,0 +1,17 @@
+(ns laba1.core
+  (:gen-class))
+
+(defn sum-nested
+  [coll]
+  (cond
+    (nil? coll) 0
+    (number? coll) coll
+    (coll? coll) (reduce + 0 (map sum-nested coll))
+    :else 0))
+
+(defn -main []
+  (println "Тст")
+  (println "[1 2 3] -" (sum-nested [1 2 3]))
+  (println "[1 [2 3] 4] -" (sum-nested [1 [2 3] 4]))
+  (println "[1 [2 [3 4]] 5] -" (sum-nested [1 [2 [3 4]] 5]))
+)
